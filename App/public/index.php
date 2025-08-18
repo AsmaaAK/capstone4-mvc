dbf2bdf886719e07103c4cec3436b1595ded1e48
AsmaaAK <asoomak97@gmail.com>
2025-08-18 09:40:41 -0700

    add .htaccess and index files

---
+++ App/public/index.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\Router;
+
+$router = new Router();
+require __DIR__ . '/../routes/web.php';
+
+$method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+$uri    = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
+
+$router->dispatch($method, $uri);
