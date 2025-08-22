755631e96f5862a29faee7f8db04fec2db972840
AsmaaAK <asoomak97@gmail.com>
2025-08-22 09:00:04 -0700

    add api router to web.php file

--- App/routes/web.php
+++ App/routes/web.php
@@ -1,16 +1,32 @@
 <?php
+declare(strict_types=1);
+
+use App\Core\Router;
 use App\Controllers\AuthController;
 use App\Controllers\UserController;
+use App\Controllers\EventController;
+use App\Controllers\MatchController;
 
+/** @var Router $router */
 
-$router->get('/volunteer-managment/public/auth/login', [AuthController::class, 'showLoginForm']);
-$router->post('/volunteer-managment/public/auth/login', [AuthController::class, 'login']);
+// Auth
+$router->add('POST',  '#^/api/register$#', [AuthController::class, 'register']);
+$router->add('POST',  '#^/api/login$#',    [AuthController::class, 'login']);
+$router->add('POST',  '#^/api/logout$#',   [AuthController::class, 'logout']);
+$router->add('GET',   '#^/api/me$#',       [AuthController::class, 'me']);
 
-$router->get('/volunteer-managment/public/auth/register', [AuthController::class, 'showRegisterForm']);
-$router->post('/volunteer-managment/public/auth/register', [AuthController::class, 'register']);
-$router->post('/volunteer-managment/public/auth/logout', [AuthController::class, 'logout']);
+// Users (Admin-only for index & destroy; show/update يسمح لصاحب الحساب)
+$router->add('GET',   '#^/api/users$#',            [UserController::class, 'index']);
+$router->add('GET',   '#^/api/users/(\d+)$#',      [UserController::class, 'show']);
+$router->add('PUT',   '#^/api/users/(\d+)$#',      [UserController::class, 'update']);
+$router->add('DELETE','#^/api/users/(\d+)$#',      [UserController::class, 'destroy']);
 
-$router->get('/volunteer-managment/public/users', [UserController::class, 'index']);
-// $router->post('/volunteer-managment/public/users', [AuthController::class, 'register']);
-// $router->get('/volunteer-managment/public/users', [AuthController::class, 'register']);
+// Events
+$router->add('GET',   '#^/api/events$#',           [EventController::class, 'index']);
+$router->add('GET',   '#^/api/events/(\d+)$#',     [EventController::class, 'show']);
+$router->add('POST',  '#^/api/events$#',           [EventController::class, 'store']);
+$router->add('PUT',   '#^/api/events/(\d+)$#',     [EventController::class, 'update']);
+$router->add('DELETE','#^/api/events/(\d+)$#',     [EventController::class, 'destroy']);
 
+// Matching
+$router->add('GET',   '#^/api/matches$#',          [MatchController::class, 'index']);
